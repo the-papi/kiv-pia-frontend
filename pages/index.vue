40ac35d8b3d6f15df5ced17920183423dfa79294
Patrik Janoušek <patrikjanousek97@gmail.com>
2020-12-06 08:53:00 +0100

    Changed lg and xl of login page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
+    <v-col cols="12" sm="8" md="6" lg="6" xl="4">
       <v-tabs
         fixed-tabs
       >
